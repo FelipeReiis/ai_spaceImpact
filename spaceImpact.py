234d11ffc9e9e5aca47888f3237c50c42890b359
Felipe <felipereis51@outlook.com>
2024-10-13 22:00:53 -0300

    quase finalizado

--- spaceImpact.py
+++ spaceImpact.py
@@ -1,9 +1,13 @@
 import pygame as pg
 import random
 import neat 
+import os
 
-LARGURA_JANELA = 500
-ALTURA_JANELA = 500
+LARGURA_JANELA = 400  # Definindo a largura fixa
+ALTURA_NAVE = 50
+ALTURA_JANELA = ALTURA_NAVE * 10  # A tela tem duas vezes o tamanho da nave
+geracao = 0
+
 class Vida():
     def __init__(self, imagem_vida, posicao):
         self.imagemVida = pg.image.load('imgs/heart.png').convert_alpha()
@@ -35,34 +39,34 @@ def colisaoNaveAlien(nave, alien):
     return nave.colliderect(alien)
 
 class Alien():
-    def __init__(self, imagens_aliens):
+    def __init__(self, imagens_aliens, posicao_nave_y):  # Apenas Y da nave é passado
         self.imgAlien = pg.image.load(random.choice(imagens_aliens)).convert_alpha()
         self.imgAlien = pg.transform.scale(self.imgAlien, (50, 50))
         self.areaAlien = self.imgAlien.get_rect()
-        self.pos_alien_x = LARGURA_JANELA  # Posição inicial fora da tela
-        self.pos_alien_y = random.randint(10, 450)
+        self.pos_alien_x = LARGURA_JANELA + 50  # Começa fora da tela (à direita)
+        self.pos_alien_y = posicao_nave_y  # A posição Y será a mesma da nave
         self.velocidade = 5.0  # Ajuste a velocidade conforme necessário
         self.tempo_atualizacao = 0
         self.tempo_atualizacao_max = 150
-        self.nave = Nave()
 
     def colocarAlienNaTela(self, item):
-        hit_box_alien = self.areaAlien
-        hit_box_alien.y = self.pos_alien_y
-        hit_box_alien.x = self.pos_alien_x
-        pg.draw.rect(item, (27, 192, 192), hit_box_alien, 4)
+        # Atualiza a hitbox do alien conforme sua posição
+        self.areaAlien.topleft = (self.pos_alien_x, self.pos_alien_y)
+        # Desenha a hitbox do alien para visualização
+        pg.draw.rect(item, (27, 192, 192), self.areaAlien, 4)  
         item.blit(self.imgAlien, (self.pos_alien_x, self.pos_alien_y))
 
     def movimentarAlien(self):
         self.pos_alien_x -= self.velocidade
-
-        # Atualizar o contador de tempo
+        # Código para aumentar a velocidade com o tempo
         self.tempo_atualizacao += 1
         if self.tempo_atualizacao >= self.tempo_atualizacao_max:
-            # Aumentar a velocidade e reiniciar o contador de tempo
             self.velocidade += 4  # Ajuste conforme necessário
             self.tempo_atualizacao = 0
-        return  self.pos_alien_x
+        return self.pos_alien_x
+
+
+
 class Bala():
     def __init__(self, x, y):
         self.imgProjetil = pg.image.load('imgs/d.png').convert_alpha()
@@ -92,189 +96,176 @@ class Nave():
         self.areaNave = self.imgNave.get_rect()
         self.pos_nave_x = 50
         self.pos_nave_y = 250
-        self.aliens = []
-        self.disparos = []
-        self.vida = True
-        self.velocidade = 5.0  # Ajuste a velocidade conforme necessário
-        self.invulneravel = False  # Adicione o atributo invulneravel
-        self.tempo_invulneravel = 0  # Adicione o atributo tempo_invulneravel
+        self.velocidade = 5.0
         self.pontuacao = 1
-
-    def disparar(self):
-        x, y = self.pos_nave_x + self.areaNave.width, self.pos_nave_y + self.areaNave.height // 2
-        bala = Bala(x, y)
-        self.disparos.append(bala)
-
+        self.aliens = []
+    
     def movimentarParaCima(self):
         if self.pos_nave_y > 10:
             self.pos_nave_y -= self.velocidade
 
-    def movimentarParaDireita(self):
-        if self.pos_nave_x < LARGURA_JANELA - self.areaNave.width:
-            self.pos_nave_x += self.velocidade
-
     def movimentarParaBaixo(self):
         if self.pos_nave_y < ALTURA_JANELA - self.areaNave.height:
             self.pos_nave_y += self.velocidade
-
-    def movimentarParaEsquerda(self):
-        if self.pos_nave_x > 1:
-            self.pos_nave_x -= self.velocidade
-
-    def colocarNaveNaTela(self, item):
-        hit_box_nave = self.areaNave
-        hit_box_nave.y = self.pos_nave_y
-        hit_box_nave.x = self.pos_nave_x
-        pg.draw.rect(item, (27, 192, 192), hit_box_nave, 4)
-        item.blit(self.imgNave, (self.pos_nave_x, self.pos_nave_y))
-
+    
     def criarAliens(self, imagens_aliens):
-        novo_alien = Alien(imagens_aliens)
+        novo_alien = Alien(imagens_aliens, self.pos_nave_y)  # Passa apenas a posição Y da nave
         self.aliens.append(novo_alien)
 
+    
     def colocarAliensNaTela(self, item):
-        for alien in self.aliens:
+        for alien in self.aliens[:]:  # Usando uma cópia da lista para evitar modificação durante a iteração
             alien.colocarAlienNaTela(item)
             alien.movimentarAlien()
-            print(alien.movimentarAlien())
-            if alien.movimentarAlien() == self.pos_nave_x:
-                self.pontuacao+=0.5
-    def verificarColisoes(self, vidas, imagens_aliens):
-        disparos_para_remover = []
-        aliens_para_remover = []
-        novos_aliens = []  # Lista para armazenar novos aliens
+            if alien.pos_alien_x < -alien.areaAlien.width:  # Se o alien sair da tela
+                self.aliens.remove(alien)  # Remover da lista
 
-        for disparo in self.disparos:
-            for alien in self.aliens:
-                if colisao(disparo.areaProjetil, alien.areaAlien):
-                    disparos_para_remover.append(disparo)
-                    aliens_para_remover.append(alien)
-                    self.pontuacao += 10
-                    
-                    
-        for disparo in disparos_para_remover:
-            if disparo in self.disparos:
-                self.disparos.remove(disparo)
-
-        for alien in aliens_para_remover:
-            if alien in self.aliens:
-                # Adicionar alien à lista de novos aliens
-                novos_aliens.append(Alien(imagens_aliens))
-                self.aliens.remove(alien)
-
-
-        # Adicionar novos aliens à lista de aliens
-        self.aliens.extend(novos_aliens)
-
-        # Verificar colisão entre a nave e os aliens
-        for alien in self.aliens:
-            if colisaoNaveAlien(self.areaNave, alien.areaAlien):
-                if not self.invulneravel:
-                    # Reposicionar a nave após a colisão
-                    self.reposicionarNave()
-                    self.invulneravel = True
-                    self.pontuacao -= 2
-                    # Remover uma vida se houver colisão
-                    if vidas:
-                        vidas.pop()
-                        self.invulneravel = False
-
-    def reposicionarNave(self):
-        self.pos_nave_x = 50
-        self.pos_nave_y = 250
+    def colocarNaveNaTela(self, item):
+        # Atualiza a hitbox da nave conforme sua posição
+        self.areaNave.topleft = (self.pos_nave_x, self.pos_nave_y)
+        # Desenha a hitbox da nave para visualização
+        pg.draw.rect(item, (0, 255, 0), self.areaNave, 2)  
+        item.blit(self.imgNave, (self.pos_nave_x, self.pos_nave_y))
 
     def obterPontuacao(self):
         return self.pontuacao
-geracao = 0
+
+    def colisaoNaveAlien(nave, alien):
+        return nave.areaNave.colliderect(alien.areaAlien)
+
+# Função principal que aplica a IA com NEAT
 def spaceImpact(genomas, config):
+    global geracao
+    geracao += 1
+    pg.init()
+
+    ALTURA_NAVE = 50
+    LARGURA_NAVE = 50
+    ALTURA_JANELA = ALTURA_NAVE * 10
+    LARGURA_JANELA = 400
+
+    tela = pg.display.set_mode((LARGURA_JANELA, ALTURA_JANELA))
+    pg.display.set_caption('Space Impact')
+
     redes = []
     lista_genomas = []
     naves = []
-    global geracao 
-    geracao+=1
-   
 
-    pg.init()
-    LARGURA_JANELA = 500
-    ALTURA_JANELA = 500
-    tela = pg.display.set_mode((LARGURA_JANELA, ALTURA_JANELA))
-    pg.display.set_caption('Space Impact')
-    jogador = Nave()
-    # alien = Alien()
     for _, genoma in genomas:
-        rede = neat.FeedForwardNetwork.create(genoma, config)
+        rede = neat.nn.FeedForwardNetwork.create(genoma, config)
         redes.append(rede)
         genoma.fitness = 0
-        lista_genomas.append(genoma)        
-        naves.append(jogador.colocarNaveNaTela(tela))
+        lista_genomas.append(genoma)
+        nave = Nave()
+        nave.ultima_posicao_y = nave.pos_nave_y
+        nave.tempo_parado = 0
+        nave.tempo_movel = 0  # Tempo que a nave está se movendo
+        naves.append(nave)
+
+    imagens_aliens = ['imgs/alien_1_1.png', 'imgs/alien_1_2.png']
     plano_fundo = pg.image.load('imgs/background_1.png').convert_alpha()
-
-    imagens_aliens = ['imgs/alien_1_1.png', 'imgs/alien_1_2.png', 'imgs/alien_2_1.png', 'imgs/alien_2_2.png', 'imgs/alien_3_1.png', 'imgs/alien_3_2.png']
-    imagem_vida = 'caminho/para/imagem_de_vida.png'  # Substitua pelo caminho real da sua imagem de vida
-
-    vidas = [Vida(imagem_vida, (LARGURA_JANELA - i * 40, 10)) for i in range(1,4)]  # Posiciona as vidas no canto superior direito
-
-    colisao_ativa = False  # Variável de controle para evitar remoção múltipla de vidas
-
     clock = pg.time.Clock()
 
-    while True:
-        if len(vidas) == 0:
-            pg.quit()
-            quit()
-        tecla = pg.key.get_pressed()
+    rodando = True
+    while rodando and len(naves) > 0:
         for evento in pg.event.get():
             if evento.type == pg.QUIT:
+                rodando = False
                 pg.quit()
                 quit()
+
         tela.blit(plano_fundo, (0, 0))
-        resto_tela = LARGURA_JANELA % plano_fundo.get_rect().width
-        tela.blit(plano_fundo,(resto_tela - plano_fundo.get_rect().width,0))
-        if resto_tela < 500:
-            tela.blit(plano_fundo,(resto_tela, 0))  
-        # Reiniciar variável de controle a cada iteração do loop
-        colisao_ativa = False
 
-        jogador.movimentarParaCima() if tecla[pg.K_UP] else None
-        jogador.movimentarParaDireita() if tecla[pg.K_RIGHT] else None
-        jogador.movimentarParaBaixo() if tecla[pg.K_DOWN] else None
-        jogador.movimentarParaEsquerda() if tecla[pg.K_LEFT] else None
-        jogador.disparar() if tecla[pg.K_SPACE] else None
+        if all(len(nave.aliens) == 0 for nave in naves):
+            for nave in naves:
+                nave.criarAliens(imagens_aliens)
 
-        # Criar novos aliens aleatoriamente
-        if random.randint(0, 100) < 2:
-            jogador.criarAliens(imagens_aliens)
+        naves_removidas = []
 
-        # Mover e exibir todos os aliens        
-        jogador.colocarAliensNaTela(tela)
-        # print(alien.pos_alien_x)
-        if len(jogador.disparos) > 0:
-            for disparo in jogador.disparos:
-                disparo.trajetoria()
-                if len(jogador.disparos) <= 5:
-                     disparo.colocarProjetilNaTela(tela)                
-                else:
-                     jogador.disparos.clear()
+        for i, nave in enumerate(naves):
+            input_data = []
+            # Obter o alien mais próximo e seus dados
+            if len(nave.aliens) > 0:
+                alien_mais_proximo = nave.aliens[0]  # Como há apenas um alien por vez
+                input_data.append(alien_mais_proximo.pos_alien_x - nave.pos_nave_x)  # Distância X
+                # input_data.append(alien_mais_proximo.pos_alien_y - nave.pos_nave_y)  # Distância Y
+                input_data.append(alien_mais_proximo.velocidade)  # Velocidade do alien
+                # input_data.append(ALTURA_NAVE) # tamanho do alien
+            else:
+                input_data += [0, 0, 0]  # Se não houver aliens, entradas são 0
 
-        LARGURA_JANELA -=1
+            input_data.append(nave.pos_nave_y)  # Posição Y da nave
 
+            # Obter a saída da rede neural
+            output = redes[i].activate(input_data)
 
-        # jogador.colocarNaveNaTela(tela)
+            # Ações da nave - inverter a lógica para verificar
+            if output[1] > output[0]:  # Move para cima
+                nave.movimentarParaCima()
+            elif output[0] > output[1]:  # Move para baixo
+                nave.movimentarParaBaixo()
 
-        # Verificar colisões
-        jogador.verificarColisoes(vidas,imagens_aliens)
+            # teclas = pg.key.get_pressed()
+            # if teclas[pg.K_UP]:  # Pressionando a tecla para cima
+            #     nave.movimentarParaCima()
+            # if teclas[pg.K_DOWN]:  # Pressionando a tecla para baixo
+            #     nave.movimentarParaBaixo()
 
-        # Exibir as vidas
-        for vida in vidas:
-            vida.colocarVidaNaTela(tela)
+            # Verificar se a nave se moveu no eixo Y
+            if nave.pos_nave_y == nave.ultima_posicao_y:
+                nave.tempo_parado += 1  # Incrementa o tempo que a nave está parada
+            else:
+                nave.tempo_parado = 0  # Reseta o tempo parado se a nave se mover
 
-        fonte = pg.font.Font(None, 36)
-        pontuacao_texto = fonte.render(f'Pontuação: {jogador.obterPontuacao()}', True, (255, 255, 255))
-        geracao_texto = fonte.render(f'Geração: {geracao}', True, (255, 255, 255))
-        tela.blit(pontuacao_texto, (10, 10))
-        tela.blit(geracao_texto, (200, 10))
-        # Atualizar a tela e controlar o FPS
+            # Aumentar fitness quando desviar de alien com sucesso
+            if alien_mais_proximo.pos_alien_x < nave.pos_nave_x and not colisaoNaveAlien(nave.areaNave, alien_mais_proximo.areaAlien):
+                lista_genomas[i].fitness += 2  # Recompensa por desvio bem-sucedido
+
+            # Punir se a nave estiver parada por mais de 1 segundo (60 frames)
+            if nave.tempo_parado > 60:  # 60 frames = 1 segundo
+                lista_genomas[i].fitness -= 0.5  # Penaliza levemente a fitness
+                nave.tempo_parado = 0  # Reseta o tempo parado após penalização
+
+            nave.ultima_posicao_y = nave.pos_nave_y  # Atualiza a última posição Y
+
+            nave.colocarNaveNaTela(tela)
+            nave.colocarAliensNaTela(tela)
+
+            # Atualizar a posição dos aliens para seguir as naves
+            for alien in nave.aliens:
+                alien.movimentarAlien()
+
+                hit_box_nave = nave.areaNave.move(nave.pos_nave_x, nave.pos_nave_y)
+                hit_box_alien = alien.areaAlien.move(alien.pos_alien_x, alien.pos_alien_y)
+
+                if colisaoNaveAlien(hit_box_nave, hit_box_alien):
+                    lista_genomas[i].fitness -= 4
+                    naves_removidas.append(nave)
+                    break
+
+        for nave_removida in naves_removidas:
+            naves.remove(nave_removida)
+
         pg.display.update()
         clock.tick(60)
 
-spaceImpact()
+    pg.quit()
+
+
+# Função principal para rodar o NEAT
+def run(config_file):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
+                          neat.DefaultStagnation, config_file)
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    estatisticas = neat.StatisticsReporter()
+    p.add_reporter(estatisticas)
+    # Definir o número de gerações para evoluir
+    p.run(spaceImpact, 100)
+
+# Chamada da função principal
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config_Ia_space_impact.txt')
+    run(config_path)
